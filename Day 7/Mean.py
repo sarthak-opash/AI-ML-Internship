6cd3a471df6145c5ac58aa96cd7954ddd6098e13
Sarthak Opash <sni.opash@gmail.com>
2026-01-09 15:34:38 +0530

    modified

--- Day 7/Mean.py
+++ Day 7/Mean.py
@@ -1,6 +1,16 @@
 import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 speed = [20,56,89,98,78,45,65,85]
 
+df = pd.DataFrame(speed)
+
 x=np.mean(speed)
 print(x)
+
+sns.kdeplot(
+    data=df
+)
+plt.show()
