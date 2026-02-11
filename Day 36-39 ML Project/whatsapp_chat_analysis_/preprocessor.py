67f3d589de46d5eea2276e436c09fa9b78378381
Sarthak Opash <sni.opash@gmail.com>
2026-02-11 13:46:42 +0530

    edited

--- Day 36-39 ML Project/whatsapp_chat_analysis_/preprocessor.py
+++ Day 36-39 ML Project/whatsapp_chat_analysis_/preprocessor.py
@@ -3,7 +3,7 @@ import re
 import pandas as pd
 
 def preprocess(data):
-    pattern = '\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{2}\s-\s'
+    pattern = r'\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{2}\s-\s'
 
     messages = re.split(pattern, data)[1:]
     dates = re.findall(pattern, data)
