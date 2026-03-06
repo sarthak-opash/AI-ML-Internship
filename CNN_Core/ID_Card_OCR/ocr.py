d9df91cf65cb9501328bd90e7aaab2d02d0618ed
Sarthak Opash <sni.opash@gmail.com>
2026-03-06 13:12:28 +0530

    edit

---
+++ CNN_Core/ID_Card_OCR/ocr.py
@@ -0,0 +1,56 @@
+import re
+import json
+import easyocr
+import numpy as np
+
+reader = easyocr.Reader(['en'])
+
+def extract_aadhar_details(image):
+
+    image = np.array(image)
+
+    results = reader.readtext(image)
+
+    text_list = []
+    for (_, text, prob) in results:
+        cleaned = text.strip()
+        text_list.append(cleaned)
+
+    full_text = " ".join(text_list)
+
+    aadhar_pattern = r'\b\d{4}\s?\d{4}\s?\d{4}\b'
+    aadhar_match = re.search(aadhar_pattern, full_text)
+
+    aadhar_number = aadhar_match.group() if aadhar_match else None
+
+    dob_pattern = r'\d{2}/\d{2}/\d{4}'
+    dob_match = re.search(dob_pattern, full_text)
+
+    dob = dob_match.group() if dob_match else None
+
+
+    name = None
+
+    for i, text in enumerate(text_list):
+
+        if any(keyword in text.lower() for keyword in [
+            "government", "india", "aadhaar", "male", "female", "dob"
+        ]):
+            continue
+
+        if re.match(r'^[A-Za-z ]{3,}$', text):
+            if dob and i < len(text_list):
+                name = text
+                break
+
+            if not dob:
+                name = text
+                break
+
+    data = {
+        "name": name,
+        "date_of_birth": dob,
+        "aadhaar_number": aadhar_number
+    }
+    
+    return json.dumps(data, indent=4)
--- Day 36-39 ML Project/whatsapp_chat_analysis_/helper.py
+++ Day 36-39 ML Project/whatsapp_chat_analysis_/helper.py
@@ -1,8 +1,8 @@
-from urlextract import URLExtract
-from wordcloud import WordCloud
-import pandas as pd
-from collections import Counter
 import emoji
+import pandas as pd
+from wordcloud import WordCloud
+from collections import Counter
+from urlextract import URLExtract
 
 extract = URLExtract()
 
