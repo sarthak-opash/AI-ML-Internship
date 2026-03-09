52d26bffd2b51b5b7195fb9eba51ed3bcc45f3cc
Sarthak Opash <sni.opash@gmail.com>
2026-03-09 15:16:45 +0530

    Your current version of Antigravity is out of date. Please visit https://antigravity.google/download to download and install the latest version.

--- CNN_Core/ID_Card_OCR/ocr.py
+++ CNN_Core/ID_Card_OCR/ocr.py
@@ -3,14 +3,12 @@ import json
 import easyocr
 import numpy as np
 
-
 reader = easyocr.Reader(['en'])
 
 def extract_aadhar_details(image):
 
     image = np.array(image)
 
-    # Run OCR
     results = reader.readtext(image)
 
     text_list = []
@@ -20,25 +18,23 @@ def extract_aadhar_details(image):
 
     full_text = " ".join(text_list)
 
-    # Aadhaar Number Extraction
     aadhar_pattern = r'\b\d{4}\s?\d{4}\s?\d{4}\b'
     aadhar_match = re.search(aadhar_pattern, full_text)
 
     aadhar_number = aadhar_match.group() if aadhar_match else None
 
-    # DOB Extraction
     dob_pattern = r'\d{2}/\d{2}/\d{4}'
     dob_match = re.search(dob_pattern, full_text)
 
     dob = dob_match.group() if dob_match else None
 
-    # Gender
+
     gender = None
+<<<<<<< HEAD
 
     for text in text_list:
-
         lower = text.lower()
-
+        
         if "male" in lower:
             gender = "Male"
             break
@@ -47,27 +43,36 @@ def extract_aadhar_details(image):
             gender = "Female"
             break
 
-    # Name Extraction
-    
+    # Name Extraction 
+    name = None
+    for text in text_list:
+=======
     name = None
 
-    for text in text_list:
+    for i, text in enumerate(text_list):
 
-        # Skip unwanted keywords
+>>>>>>> parent of 513cf796 (added-gender)
         if any(keyword in text.lower() for keyword in [
             "government", "india", "aadhaar", "male", "female", "dob"
         ]):
             continue
 
         if re.match(r'^[A-Za-z ]{3,}$', text):
-            name = text
-            break
+            if dob and i < len(text_list):
+                name = text
+                
+            if not dob and i > len(text_list):
+                gender = text
+
+            if not dob:
+                name = text
+                break
 
     data = {
         "name": name,
         "date_of_birth": dob,
-        "gender": gender,
-        "aadhaar_number": aadhar_number
+        "aadhaar_number": aadhar_number,
+        "gender": gender
     }
-
+    
     return json.dumps(data, indent=4)
